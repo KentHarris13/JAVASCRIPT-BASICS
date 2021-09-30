afdac8c2f2a3d916bde468abdab4b4ce68da7a13
Kent Harris <kentharris13@gmail.com>
2021-09-30 09:26:06 -0600

    last try plz

--- password.js
+++ password.js
@@ -1,14 +1,48 @@
-console.log('Welcome, type in a password with at least 10 charaters')
 
-// const passwordInput = required('passwordInput').create
+const readline = require("readline");
 
-function password(){
-  if (password.length >= 10){
-    console.log("Longer password required")
-  } else {
-    console.log("Login Corret")
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question('Welcome, type in a password with at least 10 charaters',function(answer){
+  if(answer.length <= 9){
+    console.log ("Try again");
   }
-}
-let passwordInput = password("asd");
-console.log(passwordInput) 
+  else
+  {
+    console.log("Correct!")
+  }
+  
+  reader.close()
+})
+
+// function checkLength(obj){
+//   var passLength = obj.length;
+//   if(passLength <= 9){
+//     return "Try again";
+//   }
+//   else
+//   {
+//     console.log("Correct!")
+//   }
+// }
+// let passwordInput = checkLength("1234567891") 
+// console.log(passwordInput)
+
+
+
+
+
+
+// function password(){
+//   if (password.length >= 10){
+//     console.log("Longer password required")
+//   } else {
+//     console.log("Login Corret")
+//   }
+// }
+// let passwordInput = password("asd");
+// console.log(passwordInput) 
 
